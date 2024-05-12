804351a5809e8330f407b0b60366645af89b9423
crystal-twinkle <autoro1@mail.ru>
2024-05-12 14:56:42 +0300

    resolve task 2

---
+++ task2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+const xmlData = fs.readFileSync('Task2.xml', 'utf8');
+
+
+xml2js.parseString(xmlData, (err, result) => {
+  if (err) {
+    console.error('Error parsing', err);
+    return;
+  }
+
+  result.root.str1[0] = result.root.str1[0].split('').reverse().join('');
+
+  result.root.str2[0] = result.root.str2[0].replace(/\d/g, '');
+
+  result.root.str3[0] = result.root.str3[0].replace(/\D/g, '');
+
+  result.root.str4[0] = `number of spaces ${result.root.str4[0].split(/\s/g).length - 1}`;
+
+  const builder = new xml2js.Builder();
+  const modifiedXmlData = builder.buildObject(result);
+
+  fs.writeFileSync('Task2_modified.xml', modifiedXmlData, 'utf8');
+});
